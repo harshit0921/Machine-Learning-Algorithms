68372567558734a3df4b5489f8139020034d5ece
harshit0921 <harshit0921@gmail.com>
2019-04-19 03:32:29 -0400

    Image processing

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -8,23 +8,23 @@ import sys
 data = pd.read_csv('SKIN_CANCER_metadata.csv')
 
 ##Read an image to get resolution
-directory = 'D:/git/HAM10000_images/' + data['Name'][0] + '.jpg'
+directory = 'D:/git/Machine-Learning-Algorithms/HAM10000_images/' + data['Name'][0] + '.jpg'
 im = Image.open(directory, 'r')
 pixels = im.size[0] * im.size[1]
 batches = 100
 
-if(int((len(data['Name']) - 15) % batches) > 0):
+if(int(len(data['Name']) % batches) > 0):
     print("Number of batches not a multiple of total data")
     sys.exit(1)
 
 #Create array to store pixel values of a batch (N x d)
-images = torch.zeros(int((len(data['Name']) - 15)/batches), pixels)
+images = torch.zeros(int(len(data['Name'])/batches), pixels)
 
 #For each batch
 for j in range(batches):
     #For each image in a batch
-    for i in range(int((len(data['Name']) - 15)/batches)):
-        directory = 'D:/git/HAM10000_images/' + data['Name'][batches*j + i] + '.jpg'
+    for i in range(int(len(data['Name'])/batches)):
+        directory = 'D:/git/Machine-Learning-Algorithms/HAM10000_images/' + data['Name'][batches*j + i] + '.jpg'
         im = Image.open(directory, 'r')
         pix_val = list(im.getdata())
         #Flatten the image and convert to grayscale values from RGB
