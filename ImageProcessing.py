46bf6654d7e22714aba90486a85eaeda97392cec
harshit0921 <harshit0921@gmail.com>
2019-04-21 00:29:10 -0400

    directory

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -4,6 +4,7 @@ from PIL import Image
 import pandas as pd
 import sys
 import os
+import math
 
 script_location = os.path.dirname(__file__)
 
@@ -12,22 +13,18 @@ data = pd.read_csv(os.path.join(script_location, 'SKIN_CANCER_metadata.csv'))
 
 ##Read an image to get resolution
 file_path = os.path.join(script_location, 'HAM10000\\' + data['Name'][0] + '.jpg')
-print(file_path)
 im = Image.open(file_path, 'r')
 pixels = im.size[0] * im.size[1]
 batches = 100
 
-if(int(len(data['Name']) % batches) > 0):
-    print("Number of batches not a multiple of total data")
-    sys.exit(1)
-
 #Create array to store pixel values of a batch (N x d)
 images = torch.zeros(int(len(data['Name'])/batches), pixels+4)
+save_file_directory = os.path.join(script_location, 'batches\\')
 
 #For each batch
 for j in range(batches):
     #For each image in a batch
-    for i in range(int(len(data['Name'])/batches)):
+    for i in range(int(math.ceil(len(data['Name'])/batches))):
         file_path = os.path.join(script_location, 'HAM10000\\' + data['Name'][batches*j + i] + '.jpg')
         #directory = '/Users/shivamodeka/Desktop/Machine-Learning-Algorithms/HAM10000/' + data['Name'][batches*j + i] + '.jpg'
         im = Image.open(file_path, 'r')
@@ -41,5 +38,5 @@ for j in range(batches):
         images[i, :] = torch.tensor(pix_val_flat)
     print("Saving file " + str(j))
     #Save the batch file
-    np.savetxt(os.path.join(script_location, 'batches\\batch' + str(j) + '.csv'), images.numpy(), delimiter = ",")
+    np.savetxt(os.path.join(save_file_directory, 'batch' + str(j) + '.csv'), images.numpy(), delimiter = ",")
 
