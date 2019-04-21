ffa24416a740bdf17b34b944bfa7948be10b53a6
Shivam Odeka <Shivam.Odeka@gmail.com>
2019-04-20 22:45:58 -0400

    merge and transform data

--- TransformData.py
+++ TransformData.py
@@ -15,7 +15,7 @@ def transform(data):
     features.extend(('Age','Male','Female'))
     x = data.loc[:, features].values
     y = data.loc[:,['Label']].values
-    x = StandardScaler().fit_transform(x)
+#    x = StandardScaler().fit_transform(x)
     pca = PCA(n_components =100)
     pc= pca.fit_transform(x)
     
---
+++ merge_data.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 20 22:05:25 2019
+
+@author: shivamodeka
+"""
+
+#from TransformData import transform
+import pandas as pd
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA 
+
+def merge():
+
+    script_location = os.path.dirname(__file__)
+    
+    l = [i for i in range(30000)]
+    l.extend(('Age','Male','Female','Label'))
+    features = [i for i in range(30000)]
+    features.extend(('Age','Male','Female'))
+    
+    data = pd.read_csv(os.path.join(script_location, 'batches/batch' + str(0) + '.csv'), names =l)
+    x = data.loc[:, features].values
+    y = data.loc[:,['Label']].values
+    
+    for i in range(1,100):
+        data = pd.read_csv(os.path.join(script_location, 'batches/batch' + str(i) + '.csv'), names =l)
+        x1 = data.loc[:, features].values
+        y1 = data.loc[:,['Label']].values
+        x= np.append(x,x1, axis =0)
+        y= np.append(y,y1, axis =0)
+        
+    x_train, x_test, y_train, y_test = train_test_split( x, y, test_size=1/5.0, random_state=0)
+    
+    print(x_train.shape, y_train.shape)
+    
+    scale = StandardScaler()
+    
+    scale.fit(x_train)
+    x_train = scale.transform(x_train)
+    x_test = scale.transform(x_test)
+    
+    pca = PCA(.99)
+    pca.fit(x_train)
+    x_train = pca.transform(x_train)
+    x_test = pca.transform(x_test)
+    
+    return x_train, x_test, y_train, y_test
